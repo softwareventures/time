63c0bd957d078c71202f9a11c6bc9d0011f95c5c
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-08-19 01:04:45 +0100

    Export consts for each month.

--- date.ts
+++ date.ts
@@ -4,3 +4,16 @@ export interface Date {
     readonly month: number;
     readonly year: number;
 }
+
+export const JANUARY = 1;
+export const FEBRUARY = 2;
+export const MARCH = 3;
+export const APRIL = 4;
+export const MAY = 5;
+export const JUNE = 6;
+export const JULY = 7;
+export const AUGUST = 8;
+export const SEPTEMBER = 9;
+export const OCTOBER = 10;
+export const NOVEMBER = 11;
+export const DECEMBER = 12;
