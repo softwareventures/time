7a36a47b8c48a06cee16145f570e9d41158e0fe0
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-12-31 18:18:22 +0000

    Implement toReferenceSeconds and fromReferenceSeconds.

--- time.ts
+++ time.ts
@@ -4,3 +4,15 @@ export interface Time {
     minutes: number;
     seconds: number;
 }
+
+export function toReferenceSeconds(time: Readonly<Time>): number {
+    return time.hours * 1440 + time.minutes * 60 + time.seconds;
+}
+
+export function fromReferenceSeconds(seconds: number): Time {
+    const hours = Math.floor(seconds / 1440);
+    seconds -= hours * 1440;
+    const minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    return {hours, minutes, seconds};
+}
