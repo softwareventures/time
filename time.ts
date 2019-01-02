3fa120f81ed8e1656115a496c839bddaf63be66d
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-01-02 01:09:37 +0000

    Fix incorrect conversion of hours to seconds.
    I am an idiot.

--- time.ts
+++ time.ts
@@ -10,12 +10,12 @@ export function normalize(time: Readonly<Time>): Time {
 }
 
 export function toReferenceSeconds(time: Readonly<Time>): number {
-    return time.hours * 1440 + time.minutes * 60 + time.seconds;
+    return time.hours * 3600 + time.minutes * 60 + time.seconds;
 }
 
 export function fromReferenceSeconds(seconds: number): Time {
     const hours = Math.floor(seconds / 1440);
-    seconds -= hours * 1440;
+    seconds -= hours * 3600;
     const minutes = Math.floor(seconds / 60);
     seconds -= minutes * 60;
     return {hours, minutes, seconds};
