67782395a56f40b279aff65977a0d5f5b7a07782
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-10 12:49:16 +0100

    feat(time): make Time type immutable
    BREAKING CHANGE: The Time type is now immutable.

--- index.ts
+++ index.ts
@@ -2,23 +2,23 @@
 export interface Time {
     /** The hours component of the time of day. Should be an integer in the
      * range 0-23. */
-    hours: number;
+    readonly hours: number;
 
     /** The minutes component of the time of day. Should be an integer in the
      * range 0-59. */
-    minutes: number;
+    readonly minutes: number;
 
     /** The seconds component of the time of day. Should be in the range 0-60,
      * inclusive of 0 but exclusive of 60. May be fractional to represent an
      * instant in time with sub-second accuracy. */
-    seconds: number;
+    readonly seconds: number;
 }
 
-export function normalize(time: Readonly<Time>): Time {
+export function normalize(time: Time): Time {
     return fromReferenceSeconds(toReferenceSeconds(time));
 }
 
-export function toReferenceSeconds(time: Readonly<Time>): number {
+export function toReferenceSeconds(time: Time): number {
     return (86400 + ((time.hours * 3600 + time.minutes * 60 + time.seconds) % 86400)) % 86400;
 }
 
